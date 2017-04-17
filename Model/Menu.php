788701f218fa8c07bf25b5620bb2eb8f3cdd1fc6
miky <mathias89100@gmail.com>
2017-04-17 03:14:28 +0200

    Merge branch 'master' of github.com:mikyprog/AdminBundle
    # Veuillez entrer un message de validation pour expliquer en quoi cette fusion est
    # nécessaire, surtout si cela fusionne une branche amont mise à jour dans une branche de sujet.
    #
    # Les lignes commençant par '#' seront ignorées, et un message vide
    # abandonne la validation.

---
+++ Model/Menu.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miky
+ * Date: 29/03/17
+ * Time: 23:24
+ */
+
+namespace Miky\Component\Menu\Model;
+
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+class Menu
+{
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var string
+     */
+    protected $positionId;
+
+    /**
+     * @var MenuItem[]
+     */
+    protected $items;
+
+    public function __construct()
+    {
+        $this->items = new ArrayCollection();
+    }
+
+    /**
+     * Add item
+     *
+     * @param MenuItem $items
+     *
+     * @return Menu
+     */
+    public function addItem(MenuItem $item)
+    {
+        $this->items[] = $item;
+
+        return $this;
+    }
+
+    /**
+     * Remove item
+     *
+     * @param MenuItem $item
+     *
+     * @return boolean TRUE if this collection contained the specified element, FALSE otherwise.
+     */
+    public function removeItem(MenuItem $item)
+    {
+        return $this->items->removeElement($item);
+    }
+
+
+    /**
+     * @return MenuItem[]
+     */
+    public function getItems()
+    {
+        return $this->items;
+    }
+
+    /**
+     * @param MenuItem[] $items
+     */
+    public function setItems($items)
+    {
+        $this->items = $items;
+    }
+
+
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPositionId()
+    {
+        return $this->positionId;
+    }
+
+    /**
+     * @param string $positionId
+     */
+    public function setPositionId($positionId)
+    {
+        $this->positionId = $positionId;
+    }
+
+
+}
---
+++ Model/MenuItem.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miky
+ * Date: 29/03/17
+ * Time: 23:23
+ */
+
+namespace Miky\Component\Menu\Model;
+
+
+class MenuItem
+{
+    /**
+     * @var Menu
+     */
+    protected $menu;
+
+    /**
+     * @return Menu
+     */
+    public function getMenu()
+    {
+        return $this->menu;
+    }
+
+    /**
+     * @param Menu $menu
+     */
+    public function setMenu($menu)
+    {
+        $this->menu = $menu;
+    }
+
+
+
+}
